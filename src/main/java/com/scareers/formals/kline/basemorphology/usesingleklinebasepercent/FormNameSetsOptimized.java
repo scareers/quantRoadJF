37a2bc09f31a3fc66dccdca3ec33c2d5b1339041
scareers <1551046542@qq.com>
2021-11-11 08:10:31 +0800

    java quantRoad -- python复刻 -- tickList不保存. bins和effectiveValueRange 随window大小自行改动

---
+++ src/main/java/com/scareers/formals/kline/basemorphology/usesingleklinebasepercent/FormNameSetsOptimized.java
@@ -0,0 +1,15 @@
+package com.scareers.formals.kline.basemorphology.usesingleklinebasepercent;
+
+import java.util.List;
+
+/**
+ * description:
+ * 1.数据流程:  主程序全数据解析 -- FilterSimple简单筛选出几十万条 -- 本脚本, 对不同参数下, 0b1s/1b2s 等的分布研究结果
+ *
+ * @author: admin
+ * @date: 2021/11/11  0011-8:06
+ */
+public class FormNameSetsOptimized {
+    public static List<List>
+
+}
