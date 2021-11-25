0ce5d9cdf3e0f6a514a6c7314f3231881ab8f529
admin <3234525484@qq.com>
2021-11-25 19:39:15 +0800

    FS -- LowBuyHighSell分析 -- 尝试模拟实验, 找到买入点期望值

--- src/main/java/com/scareers/formals/kline/basemorphology/usesingleklinebasepercent/fs/lowbuy/FSAnalyzeLowDistributionOfLowBuyNextHighSell.java
+++ src/main/java/com/scareers/formals/kline/basemorphology/usesingleklinebasepercent/fs/lowbuy/FSAnalyzeLowDistributionOfLowBuyNextHighSell.java
@@ -38,6 +38,8 @@ import static com.scareers.utils.HardwareUtils.reportCpuMemoryDisk;
  * 0 代表 09:30;  240代表 15:00 , 对应tushare分时数据, 每日 241个数据.
  * -- 分区间
  * 例如将241分钟, 中间 240间隔, 按刻分, 则分为8个区间, 规定 9:30 算作第一个区间内 30-45 算作第一个区间; 46-00 第二个
+ * <p>
+ * -Xmx512g -XX:MaxTenuringThreshold=0 -XX:GCTimeRatio=19 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10
  *
  * @author: admin
  * @date: 2021/11/14  0014-4:48
