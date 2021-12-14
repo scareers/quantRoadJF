c869bdddd3519b14d597f6cffecad079f069027b
admin <3234525484@qq.com>
2021-12-14 15:02:38 +0800

    rabbitmq -- 初识 -- 使用java api, 不整合spring

--- src/main/java/com/scareers/formals/kline/basemorphology/usesingleklinebasepercent/backtest/fs/loybuyhighsell/FSBacktestOfLowBuyNextHighSell.java
+++ src/main/java/com/scareers/formals/kline/basemorphology/usesingleklinebasepercent/backtest/fs/loybuyhighsell/FSBacktestOfLowBuyNextHighSell.java
@@ -60,7 +60,7 @@ import static com.scareers.utils.SqlUtil.execSql;
 public class FSBacktestOfLowBuyNextHighSell {
     public static void main(String[] args) throws Exception {
         reportCpuMemoryDiskSubThread(false); // 播报硬件信息
-        double[] argOfIndexLowBuys = {3.0, 5.0};
+        double[] argOfIndexLowBuys = {-5.0, -3.0, -1.0, 0.0, 1.0, 3.0, 5.0};
         double[] argOfIndexHighSells = {-5.0, -3.0, -1.0, 0.0, 1.0, 3.0, 5.0};
 
 
