8ad4be310915e196b3df136ae1d079b6c9bdefa2
admin <3234525484@qq.com>
2021-12-22 01:27:46 +0800

    fs transaction 东方财富分时成交数据

--- src/main/java/com/scareers/datasource/eastmoney/fstransaction/FSTransactionFetcher.java
+++ src/main/java/com/scareers/datasource/eastmoney/fstransaction/FSTransactionFetcher.java
@@ -231,7 +231,7 @@ public class FSTransactionFetcher {
                     "time_tick"));
             DataFrame<Object> dfTemp = dfNew.select(value -> !timeTicksOrginal.contains(value.get(2).toString()));
             DataFrame<Object> dfCurrentAll = dataOriginal.concat(dfTemp);
-            threadPoolOfSave.execute(() -> {
+            threadPoolOfSave.submit(() -> {
                 try { // 保存使用另外线程池, 不阻塞主线程池
                     DataFrameSelf.toSql(dfTemp, saveTableName, connSave, "append", null);
                 } catch (SQLException e) {
