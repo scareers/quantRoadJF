9acd71fb457717ea190b28af478adbe2f3978e84
admin <3234525484@qq.com>
2021-12-28 04:30:42 +0800

    同花顺Trader  -- em  stockApi 可使用 kevin 库请求http. 某些em api使用 hutool失败, 应当是底层设计有问题

--- src/main/java/com/scareers/formals/kline/basemorphology/usesingleklinebasepercent/backtest/fs/loybuyhighsell/parameter/IndexRealTimeRaiseFallParameter.java
+++ src/main/java/com/scareers/formals/kline/basemorphology/usesingleklinebasepercent/backtest/fs/loybuyhighsell/parameter/IndexRealTimeRaiseFallParameter.java
@@ -82,7 +82,6 @@ public class IndexRealTimeRaiseFallParameter {
 
         File file = FileUtil.file("results/IndexRealTimeRaiseFallParameter/singleTableGroupByFormsetidAvg_scale1.21.3" +
                 ".json");
-        Console.log(file.getAbsolutePath());
         FileWriter writer = new FileWriter(file);
         writer.write(JSONUtil.toJsonPrettyStr(res));
         Console.log("finish");
