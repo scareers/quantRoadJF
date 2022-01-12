b0011e38ab74cc2e7f667f02da9866a226fd56d9
admin <3234525484@qq.com>
2022-01-13 05:04:51 +0800

    同花顺Trader  -- em  stockApi 可使用 kevin 库请求http. 某些em api使用 hutool失败, 应当是底层设计有问题

--- src/main/java/com/scareers/gui/ths/simulation/interact/gui/SettingsOfGuiGlobal.java
+++ src/main/java/com/scareers/gui/ths/simulation/interact/gui/SettingsOfGuiGlobal.java
@@ -5,6 +5,7 @@ import java.awt.*;
 /**
  * description: gui 全局设定
  *
+ * @noti 路径设定, 均为资源路径. classpath下相对路径
  * @author: admin
  * @date: 2022/1/12/012-02:29:10
  */
@@ -12,10 +13,11 @@ public class SettingsOfGuiGlobal {
     public static Color colorThemeMain = new Color(43, 43, 43); // 主色,常见内容框背景色
     public static Color colorThemeMinor = new Color(60, 63, 65); // 主色2,次要颜色菜单栏,树形菜单等内容主色,主界面标题栏
     public static Color colorThemeTitle = new Color(59, 71, 84); // 主色3, 子组件标题栏颜色
-
     public static Color colorGrayCommon = new Color(187, 187, 187); // 常规灰,按钮字颜色
 
 
-    public static Color colorTest = new Color(163, 184, 204); // 主色3, 子组件标题栏颜色
+    public static Color colorTest = new Color(163, 184, 204); // 测试颜色..
+
+    public static final String MAINWINDOW_ICON_PATH = "gui/img/titleIcon0.png";
 
 }
