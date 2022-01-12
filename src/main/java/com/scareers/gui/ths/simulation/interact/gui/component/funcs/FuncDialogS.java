b0dcf7bd1dea4a522c9072c88016f3e6f2af91de
admin <3234525484@qq.com>
2022-01-13 07:32:31 +0800

    同花顺Trader  -- em  stockApi 可使用 kevin 库请求http. 某些em api使用 hutool失败, 应当是底层设计有问题

---
+++ src/main/java/com/scareers/gui/ths/simulation/interact/gui/component/funcs/FuncDialogS.java
@@ -0,0 +1,17 @@
+package com.scareers.gui.ths.simulation.interact.gui.component.funcs;
+
+import javax.swing.*;
+
+/**
+ * description: 自定义 FuncDialogS 抽象类, 逻辑意义是: 各种子功能使用的组件, 基组件是对话框
+ * 主要添加刷新bounds 的方法, 该方法从 主窗口获取信息, 并更新自身 bounds.
+ * 各个子功能的子窗口, 均需要继承此抽象类. 以便主窗口统一控制.
+ * 例如当主窗口 reSize, 则遍历所有绑定的功能对话框, 它们均调用 flushBounds(), 且重绘.
+ *
+ * @author: admin
+ * @date: 2022/1/13/013-07:27:42
+ * @see flushBounds()
+ */
+public abstract class FuncDialogS extends JDialog {
+    public abstract void flushBounds();
+}
