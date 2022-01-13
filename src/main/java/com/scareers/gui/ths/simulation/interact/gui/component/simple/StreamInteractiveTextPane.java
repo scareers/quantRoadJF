1995ef58d544aa1654c68f36d6832b7edacfe913
admin <3234525484@qq.com>
2022-01-13 10:08:04 +0800

    同花顺Trader  -- em  stockApi 可使用 kevin 库请求http. 某些em api使用 hutool失败, 应当是底层设计有问题

---
+++ src/main/java/com/scareers/gui/ths/simulation/interact/gui/component/simple/StreamInteractiveTextPane.java
@@ -0,0 +1,10 @@
+package com.scareers.gui.ths.simulation.interact.gui.component.simple;
+
+/**
+ * description: 给定 输入输出流, 达成交互! 类似 cmd shell交互. 一般用于开启其他进程后, 文本编辑框交互!
+ *
+ * @author: admin
+ * @date: 2022/1/13/013-10:06:32
+ */
+public class StreamInteractiveTextPane {
+}
