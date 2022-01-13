3ee8eff0703f3fa631ddeb0f8919bc93b0af9e09
admin <3234525484@qq.com>
2022-01-13 10:13:18 +0800

    同花顺Trader  -- em  stockApi 可使用 kevin 库请求http. 某些em api使用 hutool失败, 应当是底层设计有问题

--- src/main/java/com/scareers/gui/ths/simulation/interact/gui/component/simple/StreamInteractiveTextPaneS.java
+++ src/main/java/com/scareers/gui/ths/simulation/interact/gui/component/simple/StreamInteractiveTextPaneS.java
@@ -1,6 +1,9 @@
 package com.scareers.gui.ths.simulation.interact.gui.component.simple;
 
 import javax.swing.*;
+import javax.swing.text.StyledDocument;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * description: 给定 输入输出流, 达成交互! 类似 cmd shell交互. 一般用于开启其他进程后, 文本编辑框交互!
@@ -9,4 +12,19 @@ import javax.swing.*;
  * @date: 2022/1/13/013-10:06:32
  */
 public class StreamInteractiveTextPaneS extends JTextPane {
+    InputStream input;
+    OutputStream output;
+
+    Object lock = new Object();  // 读写共用一把锁
+
+    public StreamInteractiveTextPaneS() {
+        super();
+        this.setEditable(true);
+    }
+
+    public StreamInteractiveTextPaneS(InputStream input, OutputStream output) {
+        this();
+        this.input = input;
+        this.output = output;
+    }
 }
