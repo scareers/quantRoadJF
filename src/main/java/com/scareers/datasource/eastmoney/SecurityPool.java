67bf5aa116a074c5a1407936217702cc9685263c
admin <3234525484@qq.com>
2022-02-14 23:50:14 +0800

    同花顺Trader  -- Trader 性能优化完成

--- src/main/java/com/scareers/datasource/eastmoney/SecurityPool.java
+++ src/main/java/com/scareers/datasource/eastmoney/SecurityPool.java
@@ -1,5 +1,9 @@
 package com.scareers.datasource.eastmoney;
 
+import cn.hutool.log.Log;
+import com.scareers.utils.log.LogUtil;
+
+import java.util.Collection;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CopyOnWriteArraySet;
 
@@ -21,26 +25,64 @@ public class SecurityPool {
     public static volatile CopyOnWriteArraySet<SecurityBeanEm> allSecuritySet = new CopyOnWriteArraySet<>();
 
     /**
-     * 昨日持仓股票 + 今日选中股票(可以逻辑选,可以手动选) + 其他关心的热点股票
+     * 昨日持仓股票 + 今日选中股票(可以逻辑选,可以手动选) + 其他关心的热点股票, 买卖池可有交集, 一般其余关心池互斥二者
      */
     // 今日选股, key
-    public static volatile CopyOnWriteArraySet<SecurityBeanEm> todaySelectedStocks = new CopyOnWriteArraySet<>();//
+    public static volatile CopyOnWriteArraySet<SecurityBeanEm> todaySelectedStocks = new CopyOnWriteArraySet<>();
     // 昨日持仓,卖出
-    public static volatile CopyOnWriteArraySet<SecurityBeanEm> yesterdayHoldStocks = new CopyOnWriteArraySet<>(); //
+    public static volatile CopyOnWriteArraySet<SecurityBeanEm> yesterdayHoldStocks = new CopyOnWriteArraySet<>();
     // 其他热点股票
     public static volatile CopyOnWriteArraySet<SecurityBeanEm> otherCareStocks = new CopyOnWriteArraySet<>();
 
     /**
-     * 核心板块 及 其他关心的板块
+     * 核心板块 及 其他关心的板块, 一般互斥
      */
     // 核心板块, 往往是选股和昨日持仓股票的板块
     public static volatile CopyOnWriteArraySet<SecurityBeanEm> keyBKs = new CopyOnWriteArraySet<>();
     public static volatile CopyOnWriteArraySet<SecurityBeanEm> careBKs = new CopyOnWriteArraySet<>(); // 其他热点板块
 
     /**
-     * 核心指数 及 其他关心的指数
+     * 核心指数 及 其他关心的指数, 一般互斥
      */
     public static volatile CopyOnWriteArraySet<SecurityBeanEm> keyIndexs = new CopyOnWriteArraySet<>();
     public static volatile CopyOnWriteArraySet<SecurityBeanEm> careIndexs = new CopyOnWriteArraySet<>();
+
+    private static final Log log = LogUtil.getLogger();
+
+    /*
+     * 各项集合的添加方法.
+     *
+     * @param singleBeanOrCollection
+     */
+    public static void addToTodaySelectedStocks(SecurityBeanEm beanEm) {
+        todaySelectedStocks.add(beanEm);
+        allSecuritySet.add(beanEm);
+    }
+
+    public static void addToTodaySelectedStocks(Collection<SecurityBeanEm> beans) {
+        todaySelectedStocks.addAll(beans);
+        allSecuritySet.addAll(beans);
+    }
+
+    public static void addToYesterdayHoldStocks(SecurityBeanEm beanEm) {
+        yesterdayHoldStocks.add(beanEm);
+        allSecuritySet.add(beanEm);
+    }
+
+    public static void addToYesterdayHoldStocks(Collection<SecurityBeanEm> beans) {
+        yesterdayHoldStocks.addAll(beans);
+        allSecuritySet.addAll(beans);
+    }
+
+    public static void addToOtherCareStocks(SecurityBeanEm beanEm) {
+        otherCareStocks.add(beanEm);
+        allSecuritySet.add(beanEm);
+    }
+
+    public static void addToOtherCareStocks(Collection<SecurityBeanEm> beans) {
+        otherCareStocks.addAll(beans);
+        allSecuritySet.addAll(beans);
+    }
+
 
 }
