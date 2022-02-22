822e5e70dcd5aa6f88ced680a43e34e90dd52b49
admin <3234525484@qq.com>
2022-02-22 17:23:24 +0800

    同花顺Trader  -- 状态与因子架构 -- 复刻完成 -- 尝试gui显示 HsState

--- src/main/java/com/scareers/gui/ths/simulation/interact/gui/component/combination/state/HsStatePanel.java
+++ src/main/java/com/scareers/gui/ths/simulation/interact/gui/component/combination/state/HsStatePanel.java
@@ -7,6 +7,9 @@ import javax.swing.*;
 /**
  * description: 单个 HsState 展示面板
  *
+ * @implement 为了更加方便精细控制, 对于最简单的信息展示, 使用 GridLayout + JLabel 简单展示.
+ * @implement 对于pdf与cdf, 使用图片显示
+ * @implement 对于分时数据, 分时成交, 仅显示最新几行数据. 或其他简单显示?
  * @author: admin
  * @date: 2022/2/22/022-17:21:15
  */
--- src/main/java/com/scareers/gui/ths/simulation/strategy/adapter/state/HsState.java
+++ src/main/java/com/scareers/gui/ths/simulation/strategy/adapter/state/HsState.java
@@ -154,9 +154,7 @@ public class HsState {
      * @param state
      * @param preState
      * @return
-     * @implement 为了更加方便精细控制, 对于最简单的信息展示, 使用 GridLayout + JLabel 简单展示.
-     * @implement 对于pdf与cdf, 使用图片显示
-     * @implement 对于分时数据, 分时成交, 仅显示最新两行数据. 或其他简单显示?
+
      */
     public static HsStatePanel createPanelForHsState(HsState state, HsState preState) {
         return new HsStatePanel(state, preState);
