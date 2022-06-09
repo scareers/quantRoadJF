70355f235f1ed19f021879ab2fb1cdaff857fc11
admin <3234525484@qq.com>
2022-06-10 06:45:28 +0800

    `同花顺Trader  -- 操盘计划 -- 即将开始个股`

--- src/main/java/com/scareers/gui/ths/simulation/interact/gui/component/combination/reviewplan/bond/ReviseAccountWithOrder.java
+++ src/main/java/com/scareers/gui/ths/simulation/interact/gui/component/combination/reviewplan/bond/ReviseAccountWithOrder.java
@@ -11,13 +11,17 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * description: 复盘时模拟账号 + 订单;  账号和订单放入一个表之内, 账号相关列A开头, 订单相关列 O开头
+ * // @key3: 单条记录, 包含了账号全基本信息, 以及账号瞬间的资产状况, 以及单个订单对象!
+ * // @key3: 订单保存时, 账号状况是此前状况, 订单保存后, 才成交, 并刷新账户状态
  * 0.@update: 为了应对虚拟账号机制,对复盘开始时间的修改, 必须要求 running=false时才可进行!
+ * // @key2: 因新订单生成并保存, 应当保存新的对象, 因此实现 from(ReviseAccountWithOrder oldStare) , 复制初始状态!
  * <p>
  * 1.单次开始复盘, 重置账号!!!
  * 2.直到点击停止 ! 账号的状态保存!
  * 3.账号名称, 以 当前真实日期时间(开始时间) + 点击开始时, 的 复盘开始时间, 两者结合, 作为唯一ID;;
  * 4.只实现 买/卖功能, 视为下单后必定成交, 无撤单功能!
  * 5.买卖订单, 均以 仓位 生成订单, 而非数量, 本类实现计算方法!
+ * 6.复盘停止时, 默认以收盘价格, 卖出所有转债 !! 并生成最后一批订单;
  *
  * @author: admin
  * @date: 2022/6/9/009-12:52:58
@@ -28,7 +32,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @Table(name = "plan_of_industry_and_concept",
         indexes = {@Index(name = "dateStr_Index", columnList = "dateStr"),
                 @Index(name = "type_Index", columnList = "type")})
-public class ReviseAccount {
+public class ReviseAccountWithOrder {
     @Id
     @GeneratedValue // 默认就是auto
     @Column(name = "id")
@@ -56,17 +60,35 @@ public class ReviseAccount {
     /*
     账号持仓实时信息相关 -- 资金,持仓
      */
-    @Column(name = "price", columnDefinition = "double")
+
+    // 1.佣金配置
+    @Column(name = "commissionRate", columnDefinition = "double")
+    Double commissionRateShen = 0.00006; // 佣金率, 深市 十万分之6
+    Double commissionRateHu = 0.000002; // 佣金率, 沪市 百万分之2
+    Double commissionMinShen = 0.1; // 深市起收 1毛钱
+    Double commissionMinHu = 0.1; // 沪市起收 1毛钱
+
+    // 2.账号资金资产数据
+    @Column(name = "initMoney", columnDefinition = "double")
     Double initMoney = 10.0 * 10000; // 初始10万资金默认, 可修改
-    @Column(name = "price", columnDefinition = "double")
+    @Column(name = "cash", columnDefinition = "double")
     Double cash = 10.0 * 10000;  // 当前现金; 初始需要设置为 initMoney, 随后随着下单, 将自动增减! 持仓也会增减!
 
-    @Transient
     // 当前持仓,初始空, key为转债 转债代码, value 为数量!
-            ConcurrentHashMap<String, Integer> holdBondsMap = new ConcurrentHashMap<>();
-    @Column(name = "includeStockList", columnDefinition = "longtext") // 持仓json形式, 保存到数据库!
-            String holdBondsMapJsonStr = "{}"; // 成分股列表json字符串
-
+    @Transient
+    ConcurrentHashMap<String, Integer> holdBondsMap = new ConcurrentHashMap<>();
+    @Column(name = "holdBondsMap", columnDefinition = "longtext")
+    String holdBondsMapJsonStr = "{}"; // 成分股列表json字符串
+    // 单只资产, 今日收益, 元; key为转债代码, value 为今日最终盈利 数值! 如果还有持仓, 以最新价格计算
+    @Transient
+    ConcurrentHashMap<String, Double> bondProfitMap = new ConcurrentHashMap<>();
+    @Column(name = "bondProfitMap", columnDefinition = "longtext")
+    String bondProfitMapJsonStr = "{}";
+    // 单只资产, 当前的剩余仓位的持仓成本价格(已折算); key为转债代码, value 为当前剩余仓位的持仓成本
+    @Transient
+    ConcurrentHashMap<String, Double> bondCostPriceMap = new ConcurrentHashMap<>();
+    @Column(name = "bondCostPriceMap", columnDefinition = "longtext")
+    String bondCostPriceJsonStr = "{}";
 
     /*
     订单信息相关!
